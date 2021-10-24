6d666f09dda991a49eb291d4cb113291f9273bd9
Viet-Anh <vietaing@gmail.com>
2021-10-24 10:42:10 +0100

    wrote checkStimuli()

--- scripts/verifyVmkr.py
+++ scripts/verifyVmkr.py
@@ -1,6 +1,41 @@
 import glob
 import re
 
+def checkStimuli(filepath):
+	# note: this is asumming that the stimuli are numbered minStimulusValue, minStimulusValue+1 ... maxStimulusValue
+	minStimulusValue = 10
+	maxStimulusValue = 129
+
+	stimuli = []
+	for line in open(filepath, 'r'):
+		if re.search("Stimulus", line):
+			stimuli.append(line)
+
+	# split all lines with the commas as delimiter
+	stimuli = [line.split(',') for line in stimuli]
+	
+	# extract stimulus string
+	stimuli = [line[1] for line in stimuli] # select only second field
+	stimuli = [line.split()[-1].lstrip('S') for line in stimuli] # remove 'S' with .lstrip() and spaces with split()[-1]
+	
+	# extract stimulus integer value, remove those not in the specified range and sort
+	stimuli = [int(stimulus) for stimulus in stimuli]
+	stimuli = [stimulus for stimulus in stimuli if minStimulusValue <= stimulus and stimulus <= maxStimulusValue]
+	stimuli.sort()
+
+	# check if all the stimuli are there, exactly once
+	stimulusCheck = minStimulusValue
+	verify = True
+	for stimulus in stimuli:
+		if stimulus == stimulusCheck:
+			stimulusCheck += 1
+		else:
+			verify = False
+			break
+
+	if verify is True:
+		print("Found all stimuli in file", filepath.lstrip("../data/"))
+
 def main():
 	# searching for .vmrk files
 	filenames = glob.glob('../data/*.vmrk')
